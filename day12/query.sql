8967a38ee7dbf29fa751dd8a008e1fcdd485b451
Maddi Vasavi Geethika <mgeethika490@gmail.com>
2025-11-15 23:16:12 +0530

    Create query.sql

---
+++ day12/query.sql
@@ -0,0 +1,20 @@
+-->Day 12
+
+/*
+
+Analyze the event impact by comparing weeks with events vs weeks without events. Show: event status ('With Event' or 'No Event'), 
+count of weeks, average patient satisfaction, and average staff morale. Order by average patient satisfaction descending.
+
+*/
+
+--> Sql query
+
+select event,case when event='none' then 'No Event'
+            else 'With Event'
+            end as event_status,
+            count(week),avg(patient_satisfaction),avg(staff_morale) 
+from services_weekly
+group by event
+order by avg(patient_satisfaction) desc;
+            
+            
