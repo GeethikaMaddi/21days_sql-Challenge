75f1a69ad058d2ee5d036897f4cf89fef89611d8
Maddi Vasavi Geethika <mgeethika490@gmail.com>
2025-11-26 00:46:17 +0530

    Create query.sql

---
+++ Day19/query.sql
@@ -0,0 +1,30 @@
+-- Day 19
+
+/*
+Question:
+
+For each service, rank the weeks by patient satisfaction score (highest first). Show service, week, patient_satisfaction, patients_admitted, 
+and the rank. Include only the top 3 weeks per service.
+
+*/
+
+
+-- Sql Query
+
+
+WITH ranked AS (
+    SELECT service,week,patient_satisfaction,patients_admitted,
+        RANK() OVER (
+            PARTITION BY service
+            ORDER BY patient_satisfaction DESC
+        ) AS rnk
+    FROM services_weekly
+)
+SELECT service,week,patient_satisfaction,patients_admitted,rnk as the_rank
+FROM ranked
+WHERE rnk <= 3
+ORDER BY service, rnk, week;
+
+
+
+
